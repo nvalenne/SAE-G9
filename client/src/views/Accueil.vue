37d8b6b7671bfff3dd661410eee2c4cf2e7f60bd
nvalenne-iut90 <nathan.valenne@edu.univ-fcomte.fr>
2022-11-18 09:15:08 +0100

    structure client vue

---
+++ client/src/views/Accueil.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="accueil">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AccueilView',
+  components: {
+
+  },
+  metaInfo: {
+    title: 'Accueil'
+  }
+}
+</script>
