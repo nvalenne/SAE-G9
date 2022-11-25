e7c9a9e4f7399a97cd8699d266aaa3a9ded3ae39
redha <redha.imaloui@gmail.com>
2022-11-25 12:24:13 +0100

    méthode run / get / save / all / delete ajoutées

--- server/dao.js
+++ server/dao.js
@@ -22,4 +22,68 @@ export class AppDAO {
             client.release();
         }
     }
+
+    async run(sql,sucess_message="OK"){
+        const client = await this.db.connect();
+        try{
+            await client.query(sql)
+            console.log(sucess_message);
+        }catch (err) {
+            console.log(err)
+        }finally {
+            client.release();}
+    }
+
+    get(sql,params=[]){
+        return new Promise((resolve,reject)=>{
+            this.db.query(sql,params,(err,result)=>{
+                if(err){
+                    console.log("Erreur d'exécution de sql "+sql);
+                    console.log(err);
+                    reject(err)
+                }else {
+                    resolve(result.rows[0]); //retourner une ligne
+                }
+            })
+        });
+    }
+
+    async save(sql,params =[]){
+        const client = await this.db.connect();
+        try{
+            const res = await client.query(sql+'RETURNING *',params);
+            console.log('INSERTED: '+res.rows[0]);
+        }catch (err) {
+            console.log(err)
+        }finally {
+            client.release();}
+    }
+
+
+    all(sql,params=[]){
+        return new Promise((resolve,reject)=>{
+            this.db.query(sql,params,(err,result)=>{
+                if(err){
+                    console.log("Erreur d'exécution de sql "+sql);
+                    console.log(err);
+                    reject(err)
+                }else {
+                    resolve(result);
+                }
+            })
+        });
+    }
+
+
+    async delete(sql,params =[]){
+        const client = await this.db.connect();
+        try{
+            const res = await client.query('WITH deleted AS('+sql+'RETURNING *)'+'SELECT count(*) FROM deleted',params);
+            console.log('DELETED ROWS : '+res.rows[0].count());
+            return res.rows[0].count + 'DELETED RECORDS';
+        }catch (err) {
+            console.log(err)
+        }finally {
+            client.release();}
+    }
 }
