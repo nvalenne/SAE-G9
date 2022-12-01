c3fdc216083a50b1c3dae7b6d294e5611f4e7e57
nvalenne-iut90 <nathan.valenne@edu.univ-fcomte.fr>
2022-12-01 15:41:11 +0100

    carte Interactive v0.001

--- client/src/views/CarteView.vue
+++ client/src/views/CarteView.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="accueil">
     <NavBar/>
-    <h1>Carte normalement intéractive</h1>
+    <div id="map">
+      <MapVue/>
+    </div>
   </div>
 </template>
 
 <script>
 
 import NavBar from "@/components/NavBar";
+import MapVue from "@/components/Map.vue";
 export default {
   name: 'CarteView',
   components: {
-    NavBar
+    NavBar,
+    MapVue
 
   },
   metaInfo: {
@@ -19,3 +23,4 @@ export default {
   }
 }
 </script>
+
