ebe5437ca0ec34c1a7b1291987f63690274c7e8d
nvalenne-iut90 <nathan.valenne@edu.univ-fcomte.fr>
2022-12-08 11:00:50 +0100

    add footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-    <div></div>
+    <div>
+        <v-footer class="footer">
+            
+        </v-footer>
+    </div>
 </template>
 
 <script>
@@ -9,5 +13,11 @@ export default {
 </script>
 
 <style scoped>
-
+.footer {
+    margin-top: 300px;
+    width: 100%;
+    background-color: rgb(29, 28, 27);;
+    position: absolute;
+    height: 200px;
+}
 </style>
