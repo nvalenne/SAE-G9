ce503ff08dcbcf152fdcec0ada5355855b5f2fb6
nicolasSAE <nicolas.basquin@edu.univ-fcomte.fr>
2023-01-05 16:15:43 +0100

    vue stand

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import Editor from '@tinymce/tinymce-vue';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    components:{
+        'editor': Editor
+    }
 });
