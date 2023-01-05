fb42344600ccd16ea4f52711de2e3b817f02cec1
nvalenne-iut90 <nathan.valenne@edu.univ-fcomte.fr>
2023-01-05 17:04:53 +0100

    Amelioration visuel ListeManegeStands

--- client/src/views/ListeManegeView.vue
+++ client/src/views/ListeManegeView.vue
@@ -39,82 +39,85 @@ export default {
 <template>
   <div id="app">
     <v-container>
-      <div class="accueil">
-        <h1>Liste des manèges</h1>
-      </div>
-      <v-btn depressed color="primary">
-        <router-link to="/modifier_manege">
-          <span class="boutton">Modifier</span>
-        </router-link>
-      </v-btn>
-      <v-app id="inspire">
-        <v-text-field
-            style="width: 50%;max-height: 32px;margin-bottom: 30px;"
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Recherche"
-        ></v-text-field>
-        <div>
-          <div v-for="(attraction, index) in filterSearch" :key="index">
-            <a @click="$router.push(`/liste_manege/${attraction.id_attraction}`)">
-              <v-card elevation="4" class="mb-3">
-                <v-card-text>
-                  <v-card-title>{{attraction.nom}}</v-card-title>
-                  <v-img
-                      max-width="200px"
-                      src="https://www.petitfute.com/medias/mag/12133/835/8896-les-10-parcs-d-attraction.jpg"
-                  ></v-img>
-                  <span>Propriétaire : {{attraction.compte.nom}} {{attraction.compte.prenom}}</span><br>
-                  <span>Type d'attraction : {{attraction.type_attraction.designation}}</span><br>
-                  <span>Taille requise : {{attraction.taille_requise}}m</span><br>
-                  <span>Temps d'attente : {{attraction.attente}} min</span><br>
-                  <span>Prix enfant : {{attraction.prix_enfant}}€   -   </span>
-                  <span>Prix adulte : {{attraction.prix_adulte}}€</span>
-                </v-card-text>
-              </v-card>
-            </a>
+      <v-row>
+        <div class="col-6">
+          <div class="accueil">
+            <h1>Liste des manèges</h1>
           </div>
+          <v-btn depressed color="primary">
+            <router-link to="/modifier_manege">
+              <span class="boutton">Modifier</span>
+            </router-link>
+          </v-btn>
+          <v-app id="inspire">
+            <v-text-field
+                style="width: 50%;max-height: 32px;margin-bottom: 30px;"
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Recherche"
+            ></v-text-field>
+            <div>
+              <div v-for="(attraction, index) in filterSearch" :key="index">
+                <a @click="$router.push(`/liste_manege/${attraction.id_attraction}`)">
+                  <v-card elevation="4" class="mb-3">
+                    <v-card-title>{{attraction.nom.toUpperCase()}}</v-card-title>
+                    <v-card-text>
+                      <v-img
+                          max-width="200px"
+                          src="https://www.petitfute.com/medias/mag/12133/835/8896-les-10-parcs-d-attraction.jpg"
+                      ></v-img>
+                      <span>Propriétaire : {{attraction.compte.nom}} {{attraction.compte.prenom}}</span><br>
+                      <span>Type d'attraction : {{attraction.type_attraction.designation}}</span><br>
+                      <span>Taille requise : {{attraction.taille_requise}}m</span><br>
+                      <span>Temps d'attente : {{attraction.attente}} min</span><br>
+                      <span>Prix enfant : {{attraction.prix_enfant}}€   -   </span>
+                      <span>Prix adulte : {{attraction.prix_adulte}}€</span>
+                    </v-card-text>
+                  </v-card>
+                </a>
+              </div>
+            </div>
+          </v-app>
         </div>
-
-      </v-app>
-    </v-container>
-    <v-container>
-      <div class="accueil">
-        <h1>Liste des stands</h1>
-      </div>
-      <v-btn depressed color="primary">
-        <router-link to="/modifier_stand">
-          <span class="boutton">Modifier</span>
-        </router-link>
-      </v-btn>
-      <v-app id="inspire">
-        <v-text-field
-            style="width: 50%;max-height: 32px;margin-bottom: 30px;"
-            v-model="search2"
-            append-icon="mdi-magnify"
-            label="Recherche"
-        ></v-text-field>
-        <div>
-          <div v-for="(stand, index) in filterSearch2" :key="index">
-            <a @click="$router.push(`/liste_manege/${stand.id_stand}`)">
-              <v-card elevation="4" class="mb-3">
-                <v-card-text>
-                  <v-card-title>{{stand.nom}}</v-card-title>
-                  <v-img
-                      max-width="200px"
-                      src="https://www.petitfute.com/medias/mag/12133/835/8896-les-10-parcs-d-attraction.jpg"
-                  ></v-img>
-                  <span>Propriétaire : {{stand.compte.nom}} {{stand.compte.prenom}}</span><br>
-                  <span>Type de stand : {{stand.type_stand.designation}}</span><br>
-                  <span>Temps d'attente : {{stand.attente}} min</span><br>
-                  <span>Prix : {{stand.prix}}€</span>
-                </v-card-text>
-              </v-card>
-            </a>
+        <div class="col-6">
+          <div class="accueil">
+            <h1>Liste des stands</h1>
           </div>
-        </div>
+          <v-btn depressed color="primary">
+            <router-link to="/modifier_stand">
+              <span class="boutton">Modifier</span>
+            </router-link>
+          </v-btn>
+          <v-app id="inspire">
+            <v-text-field
+                style="width: 50%;max-height: 32px;margin-bottom: 30px;"
+                v-model="search2"
+                append-icon="mdi-magnify"
+                label="Recherche"
+            ></v-text-field>
+            <div>
+              <div v-for="(stand, index) in filterSearch2" :key="index">
+                <a @click="$router.push(`/liste_manege/${stand.id_stand}`)">
+                  <v-card elevation="4" class="mb-3">
+                    <v-card-title>{{stand.nom.toUpperCase()}}</v-card-title>
+                    <v-card-text>
+                      <v-img
+                          max-width="200px"
+                          src="https://www.animation-evenement-ville.fr/wp-content/uploads/2020/11/LE-CHAMBOULTOUT-FORAIN-VIGNETTE-SITE.jpg"
+                      ></v-img>
+                      <span>Propriétaire : {{stand.compte.nom}} {{stand.compte.prenom}}</span><br>
+                      <span>Type de stand : {{stand.type_stand.designation}}</span><br>
+                      <span>Temps d'attente : {{stand.attente}} min</span><br>
+                      <span>Prix : {{stand.prix}}€</span>
+                    </v-card-text>
+                  </v-card>
+                </a>
+              </div>
+            </div>
 
-      </v-app>
+          </v-app>
+        </div>
+      </v-row>
     </v-container>
   </div>
 </template>
